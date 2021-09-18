f4f19b6ab26c0ed8be65729012e9fdd3c394e200
Kali <kali@kali>
2021-09-17 20:42:02 -0400

    9/17/21

--- server.py
+++ server.py
@@ -20,7 +20,8 @@ def handle_client(client_socket):
     with client_socket as sock:
         request = sock.recv(1024)
         print(f'[*] Received: {request.decode("utf-8")}')
-        sock.send(b'ACK')
+        #sock.send(b'ACK')
+        sock.send(b'HTTP/1.1 200 OK')
 
 if __name__ == '__main__':
     main()
